88fcd55408be1457f588c9ca2bb43761990a5c62
renatorv <renatorv@gmail.com>
2022-07-07 08:16:59 -0300

    primeiro commit

---
+++ bin/tratamentoerros.dart
@@ -0,0 +1,5 @@
+import 'package:tratamentoerros/tratamentoerros.dart' as tratamentoerros;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tratamentoerros.calculate()}!');
+}
---
+++ test/tratamentoerros_test.dart
@@ -0,0 +1,8 @@
+import 'package:tratamentoerros/tratamentoerros.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
